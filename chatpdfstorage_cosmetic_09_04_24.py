9e88caa0c5f3bffe37a4711a04cf8ace1f018faf
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2025-05-06 09:24:12 +0530

    Update chatpdfstorage_cosmetic_09_04_24.py

--- chatpdfstorage_cosmetic_09_04_24.py
+++ chatpdfstorage_cosmetic_09_04_24.py
@@ -4,7 +4,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
