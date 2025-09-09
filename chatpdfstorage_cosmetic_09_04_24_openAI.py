d28261f1704afbb7e818c8aca205248c056fc445
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2025-09-09 09:32:51 +0530

    Comment out Google Generative AI import
    Comment out unused import for Google Generative AI.

--- chatpdfstorage_cosmetic_09_04_24_openAI.py
+++ chatpdfstorage_cosmetic_09_04_24_openAI.py
@@ -3,7 +3,7 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 #from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
+#import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 #from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
